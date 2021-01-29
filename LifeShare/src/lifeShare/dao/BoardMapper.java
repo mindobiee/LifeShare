e2ed96b447a2d18d104b30f1d287faefb1fb9982
mindobiee <mk1795@naver.com>
2021-01-29 10:16:57 +0900

    classpath, board vo,mapper,service 기본내용 추가

--- LifeShare/src/lifeShare/dao/BoardMapper.java
+++ LifeShare/src/lifeShare/dao/BoardMapper.java
@@ -1,8 +1,18 @@
 package lifeShare.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
+import lifeShare.dto.Board;
+
 @Mapper
-public class BoardMapper {
+public interface BoardMapper {
+	
+	public Board getBoard(int i); 		//아이디의 게시글 찾아오기
+	public void addBoard(Board board); 	//게시글 추가하기
+	public List<Board> getBoards(); 	//게시글 모두 가져오기
+	public void updateBoard(Board board);//게시글 업데이트 
+	public void deleteBoard(Board board);//게시글 삭제하기
 
 }
--- LifeShare/src/lifeShare/service/BoardService.java
+++ LifeShare/src/lifeShare/service/BoardService.java
@@ -1,8 +1,32 @@
 package lifeShare.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import lifeShare.dao.BoardMapper;
+import lifeShare.dto.Board;
 
 @Service
 public class BoardService {
 
+	@Autowired
+	private BoardMapper boardMapper;
+	
+	public void addBoard(Board board) {	
+		boardMapper.addBoard(board);
+	}
+	
+	public List<Board> getBoards(){
+		return boardMapper.getBoards();
+	}
+	
+	public Board getBoard(int id) {
+		return boardMapper.getBoard(id);
+	}
+	
+	public void updateBoard(Board board) {
+		boardMapper.updateBoard(board);
+	}
 }
