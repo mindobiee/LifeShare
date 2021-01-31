aca6c82a6bfe39703db18a737f28f0db263ac98d
고종윤 <>
2021-01-31 14:21:08 +0900

    update) likes 기능 구현 진행중

--- LifeShare/src/lifeShare/controller/LikesController.java
+++ LifeShare/src/lifeShare/controller/LikesController.java
@@ -1,10 +1,31 @@
 package lifeShare.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import lifeShare.dao.LikesMapper;
+import lifeShare.dao.UsersMapper;
+import lifeShare.dto.Likes;
+import lifeShare.dto.Users;
+import lifeShare.service.LikesService;
 
 @Controller 
 @RequestMapping(path = "/likes" )
 public class LikesController {
-
+	
+	@Autowired
+	private LikesService likseService; 
+	
+	@GetMapping
+	public void addLike(String uid,int bid) {
+		likseService.addLike(uid,bid);
+	}
+	
+	
 }
--- LifeShare/src/lifeShare/service/LiksesService.java
+++
@@ -1,8 +0,0 @@
-package lifeShare.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class LiksesService {
-
-}
